3ad3bfd3d8902e4ce15c33ed9d662ff7460fc2c0
ItsXete <[dgiabach2000@gmail.com]>
2025-07-24 16:45:52 -0400

    users refactored

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -18,15 +18,23 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = function(email) {
+  return pool
+    .query(
+      `SELECT * FROM users
+      WHERE LOWER(email) = LOWER($1)
+      LIMIT 1;
+      `,
+      [email]
+    )
+    .then((res) => {
+      // Return the user object if found, or null if no rows returned
+      return res.rows[0] || null;
+    })
+    .catch((err) => {
+      console.error("query error", err.stack);
+      throw err;
+    });
 };
 
 /**
@@ -35,19 +43,34 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
-};
+  return pool
+    .query(
+      `SELECT * FROM users WHERE id = $1 LIMIT 1;`,
+      [id]
+    )
+    .then(res => res.rows[0] || null)
+    .catch(err => {
+      console.error("query error", err.stack);
+      throw err;
+    });
+}; // Exact setup as getUserWithEmail
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = function(user) {
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(queryString, values)
+    .then(result => Promise.resolve(result.rows[0]))
+    .catch(err => Promise.reject(err));
 };
 
 /// Reservations
